5f08f22d3be9ce9491de0b219bad3d86d5449301
Darwin <darwin78913@gmail.com>
2023-05-06 18:23:20 -0400

    endpoint supports pod queries

--- herodotus_endpoint/endpoint.py
+++ herodotus_endpoint/endpoint.py
@@ -1,14 +1,12 @@
 #!/usr/local/bin/python3
 
-from termcolor import cprint, colored
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 from typing import Tuple, Union
+from collections import defaultdict
 
-import argparse
 import os
 import requests
-import sys
 import re
 import json
 import logging
@@ -33,9 +31,10 @@ NODE_FILTER_PASS_RE = r'plugin="([^"]+)"} (\d+)'
 NODE_FILTER_ATTEMPTS_RE = r'plugin="([^"]+)"} (\d+)'
 NODE_SCORE_ATTEMPTS_RE = r'node="[^"]+"} (\d+)'
 NODE_SCORE_TOTAL_RE = r'node="[^"]+"} (\d+)'
-NODE_SCORE_PER_POD_RE = r''
+NODE_SCORE_PER_POD_RE = r'node="([^"]+)",plugin="([^"]+)",' + 'pod="kubernetes.io/herodotus-scheduler/{namespace}/{pod_name}"}}' + r' (\d+)'
+NODE_FILTER_STATUS_PER_POD_RE = r'node="([^"]+)",plugin="([^"]+)",' + 'pod="kubernetes.io/herodotus-scheduler/{namespace}/{pod_name}"}}' + r' (\d+)'
 
-NO_NODE_FOUND_ERROR = 0
+NOT_FOUND_ERROR = 0
 REGEX_ERROR_ERROR = 1
 
 # scheduler_node_filter_status{node="microk8s-node-1",plugin="VolumeZone",pod="kubernetes.io/herodotus-scheduler/default/annotation-default-scheduler"} 1
@@ -64,6 +63,9 @@ class HerodotusEndpoint(BaseHTTPRequestHandler):
 
     def _send_text(self, text: str):
         self.wfile.write(bytes(text, encoding="utf-8"))
+
+    def _get_pod_key(self, pod: str, namespace: str):
+        return f"kubernetes.io/herodotus-scheduler/{namespace}/{pod}"
 
     def _handle_node_request(self, request: requests.Response, node_name: str) -> Tuple[Union[dict, int], Union[str, None]]:
         node_filter_pass = node_filter_attempts = dict()
@@ -100,7 +102,7 @@ class HerodotusEndpoint(BaseHTTPRequestHandler):
                 node_score_total = int(search.group(1))
 
         if not found_node:
-            return NO_NODE_FOUND_ERROR, f"No node found with name {node_name}"
+            return NOT_FOUND_ERROR, f"No node found with name {node_name}"
 
         return {
             'node_score_total': node_score_total,
@@ -110,12 +112,31 @@ class HerodotusEndpoint(BaseHTTPRequestHandler):
         }, None
     
     def _handle_pod_request(self, request: requests.Response, pod_name: str, namespace: str) -> Tuple[Union[dict, int], Union[str, None]]:
+        pod_filter_scores = pod_filter_status = defaultdict(lambda: defaultdict(dict))
+        found_pod = False
         for line in request.text.splitlines():
             if line.startswith(NODE_SCORE_PER_POD_PREFIX):
-                pass
-            elif line.startswith(NODE_FILTER_STATUS_PREFIX):
-                pass
+                print(line)
+                search = re.search(NODE_SCORE_PER_POD_RE.format(namespace=namespace, pod_name=pod_name), line)
+                if search:
+                    # arbitarily use pod score per filter to mark whether a pod has been found but any should work
+                    found_pod = True 
 
+                    pod_filter_scores[pod_name][search.group(1)][search.group(2)] = float(search.group(3))
+            elif line.startswith(NODE_FILTER_STATUS_PREFIX):
+                print(line)
+                search = re.search(NODE_FILTER_STATUS_PER_POD_RE.format(namespace=namespace, pod_name=pod_name), line)
+                if search:
+                    pod_filter_status[pod_name][search.group(1)][search.group(2)] = int(search.group(3))
+        
+        if not found_pod:
+            return NOT_FOUND_ERROR, f"No pod found with name {pod_name} under namespace {namespace}"
+        
+        return {
+            "filter_scores": pod_filter_scores,
+            "filter_status": pod_filter_status,
+        }, None
+    
     def do_GET(self):
         sched_addr = os.environ[SCHED_ADDR_ENV]
         sched_port = os.environ[SCHED_PORT_ENV]
@@ -144,15 +165,30 @@ class HerodotusEndpoint(BaseHTTPRequestHandler):
                 self._send_header(200)
                 self._send_text(json.dumps(out))
             else:
-                if out == NO_NODE_FOUND_ERROR:
-                    self._send_header(404)
+                if out == NOT_FOUND_ERROR:
+                    self._send_header(404, type="text/plain")
                 else:
                     self._send_header(400, type="text/plain")
                 self._send_text(err)
         elif parse.path.lower() == "/pod":
-            pass
+            if 'namespace' not in query_comp.keys():
+                self._send_header(400, type="text/plain")
+                self._send_text("No namespace given for query")
+                return
+            
+            out, err = self._handle_pod_request(r, query_comp['name'][0], query_comp['namespace'][0])
+            if err == None:
+                self._send_header(200)
+                self._send_text(json.dumps(out))
+            else:
+                if out == NOT_FOUND_ERROR:
+                    self._send_header(404, type="text/plain")
+                else:
+                    self._send_header(400, type="text/plain")
+                self._send_text(err)   
         else:
-            self._send_header(400)
+            self._send_header(400, type="text/plain")
+            self._send_text("Bad text")
 
 
 if __name__ == "__main__":
